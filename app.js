d481709c4cbc23c416fc6d891a6149c9efa2d21d
Salim Salimzada <salimsalimzada2002@gmail.com>
2024-02-11 01:04:41 +0400

    some cool stuff

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const sayHi = () => {
 }
 
 console.log('hello,world')
+
+//  write fibonacci sequence finder with tail call optimization
+
+const fibonacci = (n, cache = {}) => {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+
+    if (cache[n]) {
+        return cache[n];
+    }
+
+    const result = fibonacci(n - 1, cache) + fibonacci(n - 2, cache);
+    cache[n] = result;
+
+    return result;
+};
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('some flushy console');
