0539ad6191202e4e7153bd8e0921891128e878f4
Salim Salimzada <salimsalimzada2002@gmail.com>
2024-02-11 02:47:35 +0400

    Add store.js

---
+++ store.js
@@ -0,0 +1,18 @@
+// implement basic store
+// 1. getState
+// 2. dispatch
+// 3. subscribe
+// 4. unsubscribe
+// 5. reducer
+// 6. createStore
+// 7. combineReducers
+// 8. applyMiddleware
+// 9. compose
+// 10. bindActionCreators
+// 11. createStoreWithMiddleware
+// 12. applyMiddlewareWithObj
+
+// Path: store.js
+function getState() {
+  return this.state;
+}
